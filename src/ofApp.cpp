a4fb17f9445ac7fe7978fb4180a3c06b4ce14cf6
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2022-11-24 01:42:26 +0900

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,56 +1,61 @@
 #include "ofApp.h"
 
-float loc_x;//circle's x-coordinate
-float loc_y;//circle's y-coordinate
-float speed_x;//speed for x-coordinate direction
-float speed_y;//speed for y-coordinate direction
+static const int NUM = 100;
+
+float loc_x[NUM];//circle's x-coordinate
+float loc_y[NUM];//circle's y-coordinate
+float speed_x[NUM];//speed for  x-coordinate direction
+float speed_y[NUM];//speed for  y-coordinate direction
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofBackground(0, 0, 0);
     ofSetFrameRate(60);
     ofSetCircleResolution(64);
+    ofEnableAlphaBlending();
 
-    loc_x = 20; //first point of circle's x-coordinate
-    loc_y = 20; //first point of circle's y-coordinate
-    speed_x = 4; //first point of speed for x-coordinate direction
-    speed_y = 7; //first point of speed for y-coordinate direction
+    //loop until num number
+    for(int i = 0; i < NUM; i++){
+        loc_x[i] = ofRandom(0,ofGetWidth());
+        loc_y[i] = ofRandom(0,ofGetHeight());
+        speed_x[i] = ofRandom(-10,10);
+        speed_y[i] = ofRandom(-10,10);
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    loc_x = loc_x + speed_x;//Update for circle's x-coordinate
-    loc_y = loc_y + speed_y;//Update for circle's y-coordinate
+    //loop num coordinate number
+    for(int i =0; i < NUM; i++){
+        loc_x[i] = loc_x[i] + speed_x[i];//Update for circle's x-coordinate
+        loc_y[i] = loc_y[i]+ speed_y[i];//Update for circle's y-coordinate
 
-    //condition 1 bounce off the left side
-    if(loc_x < 0){
-        speed_x = speed_x * -1;
+        //condition 1 bounce off the left side
+        if(loc_x[i] < 0){
+            speed_x[i] = speed_x[i] * -1;
+        }
+
+        if(loc_x[i] > ofGetWidth()){
+            speed_x[i] = speed_x[i] * -1;
+        }
+
+        if(loc_y[i] < 0){
+            speed_y[i] = speed_y[i] * -1;
+        }
+        if(loc_y[i] > ofGetWidth()){
+            speed_y[i] = speed_y[i] * -1;
+        }
     }
-
-    //condition 2 bounce off the right side
-    if(loc_x > ofGetWidth()){
-        speed_x = speed_x * -1;
-    }
-
-    //condition 3 bounce off the upside
-    if(loc_y < 0){
-        speed_y = speed_y * -1;
-    }
-
-    //condition 4 bounce off the bottom side
-    if(loc_y > ofGetHeight()){
-        speed_y = speed_y * -1;
-    }
-
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofSetColor(255, 0, 255);
-    ofDrawCircle(loc_x, loc_y, 60);
+    ofSetColor(255, 102, 255, 127);
+    for(int i = 0; i < NUM; i++){
+        ofDrawCircle(loc_x[i], loc_y[i], 40);
+    }
 }
-
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
